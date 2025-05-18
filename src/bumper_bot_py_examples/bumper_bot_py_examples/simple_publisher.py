cf09d9710d70ba87d4afb32efdabf1431d9425e5
Joel Jebitto <joeljebitto@gmail.com>
2025-05-18 16:26:53 +0530

    added simple python publisher

---
+++ src/bumper_bot_py_examples/bumper_bot_py_examples/simple_publisher.py
@@ -0,0 +1,33 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+class SimplePublisher(Node):
+    def __init__(self):
+        super().__init__("simple_publisher")
+
+        self.pub_ = self.create_publisher(String, "chatter", 10)
+        self.counter_ = 0
+        self.frequency_ = 1.0
+        self.get_logger().info(f"publishing at {self.frequency_}hz")
+        self.timer_ = self.create_timer(self.frequency_, self.timerCallback)
+
+    def timerCallback(self):
+        msg = String()
+        msg.data = "Hello ROS 2 - counter: %d" % self.counter_
+
+        self.pub_.publish(msg)
+        self.counter_ += 1
+
+
+def main():
+    rclpy.init()
+    publisher = SimplePublisher()
+    rclpy.spin(publisher)
+    publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
--- src/bumper_bot_py_examples/setup.py
+++ src/bumper_bot_py_examples/setup.py
@@ -1,25 +1,26 @@
 from setuptools import find_packages, setup
 
-package_name = 'bumper_bot_py_examples'
+package_name = "bumper_bot_py_examples"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages",
+         ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='joeljebitto',
-    maintainer_email='joeljebitto@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="joeljebitto",
+    maintainer_email="joeljebitto@gmail.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
+            "simple_publisher = bumper_bot_py_examples.simple_publisher:main"
         ],
     },
 )
